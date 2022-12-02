e92227b7040a973c52d463e7b833e5261f100b79
BombshellTom <bombshell.tom@gmail.com>
2022-12-02 15:47:58 +0000

    stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game loop, called when the script is first loaded 
  * and then after the users answer has been processed.
  */
-
-function runGame() {  
+function runGame(gameType) {  
     // creates two random numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "additon") {
+    displayAdditionQuestion(num1, num2)   
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${'gameType}. Aborting`);
+    }
 }
 
 function checkAnswer() {
